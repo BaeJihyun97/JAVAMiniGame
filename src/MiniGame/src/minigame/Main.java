224e54c27af4763c5ed69539f30d2ebc27ae573e
BaeJihyun97 <baejihyun970519@gmail.com>
2022-05-09 16:04:23 +0900

    main function

--- src/MiniGame/src/minigame/Main.java
+++ src/MiniGame/src/minigame/Main.java
@@ -9,17 +9,18 @@ public class Main extends PageManager{
 		
 		while(!finish) {
 			int pageN = PageManager.page;
-			while(pageN != PageManager.page) {
+					
+			if(PageManager.page == 1) page1.setvisibility(true);
+			if(PageManager.page == 2) page2.setvisibility(true);
+			
+			while(pageN == PageManager.page) {
 				try {
-					Thread.sleep(100);
+					Thread.sleep(500);
 				}
 				catch (InterruptedException e){
 					e.printStackTrace();
 				}
 			}
-			
-			if(PageManager.page == 1) page1.setvisibility(true);
-			if(PageManager.page == 2) page2.setvisibility(true);
 		}
 	}
 }
