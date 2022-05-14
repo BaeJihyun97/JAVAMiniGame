9f3ccc44bd9f7bf00756e2c203e404689119a7f0
swjjnew <103331777+swjjnew@users.noreply.github.com>
2022-05-14 23:03:59 +0900

    Update Runners.java

--- src/MiniGame/src/minigame/Runners.java
+++ src/MiniGame/src/minigame/Runners.java
@@ -1,7 +1,6 @@
 package minigame;
 
 public class Runners {
-	int[] track;
 	int location;
 	boolean running;
 	int track_num;
@@ -9,17 +8,10 @@ public class Runners {
 		this.track_num = track_num;
 		this.running = true;
 		this.location = 0;
-		this.track = new int[track_num];
-		for(int i=0; i<track_num; i++) {
-			this.track[i] = 1;
-		}
-		this.track[this.location] = 0;
 	}
 	
 	public void Run() {
 		this.location += 1;
-		this.track[this.location] = 0;
-		this.track[this.location-1] = -1;
 		if(this.location==(track_num-1)) {
 			this.running = false;
 		}
