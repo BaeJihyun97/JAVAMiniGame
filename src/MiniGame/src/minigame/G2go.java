65a3adf2734ad10788f9fbfdc472943990691aa0
BaeJihyun97 <baejihyun970519@gmail.com>
2022-05-22 11:35:35 +0900

    socket complete

--- src/MiniGame/src/minigame/G2go.java
+++ src/MiniGame/src/minigame/G2go.java
@@ -1,24 +1,43 @@
 package minigame;
 
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Font;
-import java.awt.GridBagLayout;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
+import javax.swing.*;
+
 
 @SuppressWarnings("serial")
 public class G2go extends ComponentManger{
 	JPanel grid_panel;
-	GridBagLayout gBag;
+	GridBagLayout gBag, go_gBag;
 	boolean isvisible = false;
+	JButton[] gobuttonL = new JButton[361];
+	Client client;
+	char[] stateArr = new char[361];
+	char ustate = '0'; //1=white, 2=black
+	char turn = '0';
+	int errorN = 0;
 	
-	public G2go() {
+	
+//	JPanel go_grid_panel = new JPanel();
+	JPanel go_grid_panel=new JPanel() {		
+		ImageIcon background = imageSetSize("./image/g2go.png", 800, 800);
+		@Override
+		protected void paintComponent(Graphics g) {
+		    super.paintComponent(g);
+		    g.drawImage(background.getImage(), 0, 0, this); 
+		}		
+	};
+	
+	static int pi;
+	
+	
+	public G2go(Client client) {
+		this.client = client;
 		setTitle("G2go");
 		setExtendedState(JFrame.MAXIMIZED_BOTH);
-		setMinimumSize(new Dimension(800, 600));
+		setMinimumSize(new Dimension(900, 900));
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBackground(Color.WHITE);
 		
@@ -27,13 +46,110 @@ public class G2go extends ComponentManger{
 		grid_panel.setBackground(Color.WHITE);
 	    grid_panel.setLayout(gBag);
 	    
-	    JLabel waiting = makeLabel(new JLabel("GAME2 TMP PAGE",JLabel.LEFT), new Font(null, Font.BOLD, 100), Color.BLACK, null);
-	    gbinsert(grid_panel, waiting, 0, 0, 2, 2);
+	    go_gBag = new GridBagLayout();
+	    go_grid_panel.setBackground(Color.WHITE);
+	    go_grid_panel.setLayout(go_gBag);
+	    go_grid_panel.setPreferredSize(new Dimension(800, 800));
+	    //go_grid_panel.setLayout(null);
+	    
+	    
+	    for (int i=0; i < 361; i++) {
+	    	gobuttonL[i] = makeJButton(new JButton( Integer.toString(i)), new Font(null, Font.BOLD, 5), null, Color.WHITE, new Dimension(16, 16));
+	    	gobuttonL[i].setFocusPainted(false);
+	    	gobuttonL[i].setBorderPainted(false);
+	    	gobuttonL[i].setOpaque(false);
+	    	gbinsert(go_grid_panel, gobuttonL[i], (i % 19), (i / 19), 1, 1, new Insets(13, 13, 13, 13));
+	    	
+	    	gobuttonL[i].addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					switchbutton(e);
+					sendpackets();
+				}
+			});
+	    	stateArr[i] = '0';
+	    }
+	   
+	        
+	   
+	    gbinsert(grid_panel, go_grid_panel, 0, 0, 2, 2);
 	    add("Center", grid_panel);
+	}
+	
+	void switchbutton(ActionEvent e) {
+		if(this.turn == this.ustate && this.turn != '0'){
+			int i = Integer.parseInt(((JButton)e.getSource()).getText());
+			this.stateArr[i] = this.ustate;
+			updateGo();
+		}	
+	}
+	
+	void updateGo() {	
+		for(int j=0; j < 361; j++) {
+			String path;
+			if(this.stateArr[j] != '0') {
+				go_grid_panel.remove(gobuttonL[j]);
+				path = (this.stateArr[j]=='1'?"./image/circle_w.png":"./image/circle_b.png");
+				JLabel picLabel = new JLabel( imageSetSize(path, 24, 24) );
+				gbinsert(go_grid_panel, picLabel, (j % 19), (j / 19), 1, 1, new Insets(4, 4, 4, 4));
+				System.out.println(j + " " +this.stateArr[j]);
+			}
+			
+		}
+		
+		
+		go_grid_panel.revalidate();
+		go_grid_panel.repaint();
 	}
 	
 	public void setvisibility(boolean isvisible) {
     	this.isvisible = isvisible;
     	this.setVisible(isvisible);
     }
+	
+	public void sendpackets() {
+		if(this.turn == this.ustate && this.turn != '0'){
+			String msg = String.valueOf(this.turn) + String.valueOf(this.ustate) + String.valueOf(stateArr);
+			//System.out.println("Clinet:: turn " + this.turn + " ustate " + this.ustate);
+			//System.out.println(msg);
+			client.sendMessage( msg, PageManager.id, "2003");
+		}
+		this.turn = '0';
+	}
+	
+	public void getpackets() {
+		String msg = client.receiveMessage();
+		//System.out.println("read packet" + " " + PageManager.code + PageManager.code.equals("2001") + msg.equals("start"));
+		if(msg == null) this.errorN += 1;
+		if(PageManager.code.equals("2001") && msg.equals("start")) {
+			PageManager.page = 6; 
+			System.out.println("user game2 start");
+			if(this.ustate == '1') this.turn = '1';
+		}
+		if(PageManager.code.equals("2002") && msg.equals("end")) PageManager.page = 1;
+		if(PageManager.code.equals("2003")) {
+			char[] msgstate = msg.toCharArray();
+			this.turn = msgstate[0];
+			System.out.println("Clinet:: turn " + this.turn + " ustate " + this.ustate);
+			for(int i=0; i<361; i++) {
+				this.stateArr[i] = msgstate[i+2];
+			}
+			updateGo();
+		}
+		//implement update
+		//implement game check
+	}
+	
+	public void G2Run() {
+		
+		this.ustate = PageManager.state;
+		System.out.println("user : " + this.ustate + " game2 waiting...");
+		while(!PageManager.over) {
+			getpackets();
+			if(this.errorN > 10) break;
+		}
+		System.out.println("game over");
+	}
+	//packet: turn/user/stateArr
+	
 }
--- src/MiniGame/src/minigame/Server.java
+++ src/MiniGame/src/minigame/Server.java
@@ -7,17 +7,26 @@ import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.channels.*;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Set;
 
 
 import java.nio.ByteBuffer;
 
-
-public class Server {
+@SuppressWarnings("serial")
+public class Server extends PageManager{
 	Selector selector;
 	ServerSocketChannel serverSocket;
 	ByteBuffer buffer;
+	ArrayList<String> users = new ArrayList<String>();
+	static int count = 0;
+	String code = "";
+	char[] stateArr = new char[361];
+	char turn = '0';
+	int gamestate = 0;
+	int done = 0;
+	String curuser;
 	
 	public Server() {
 		
@@ -30,45 +39,22 @@ public class Server {
         serverSocket.configureBlocking(false);
         serverSocket.register(selector, SelectionKey.OP_ACCEPT);
         buffer = ByteBuffer.allocate(2048);
-
-        while (true) {
-            selector.select();
-            Set<SelectionKey> selectedKeys = selector.selectedKeys();
-            Iterator<SelectionKey> iter = selectedKeys.iterator();
-            while (iter.hasNext()) {
-
-                SelectionKey key = iter.next();
-
-                if (key.isAcceptable()) {
-                    register(selector, serverSocket);
-                }
-
-                if (key.isReadable()) {
-                	//have to change this part
-                    answerWithEcho(buffer, key);
-                }
-                iter.remove();
-            }
+        for(int i=0; i < 361; i++) {
+        	stateArr[i] = '0';
         }
 
 	}
 	
-	private void answerWithEcho(ByteBuffer buffer, SelectionKey key) throws IOException {
-		 
-	        SocketChannel client = (SocketChannel) key.channel();
-	        readChunkData(buffer, client);
-	        
-	        
-	        //client.read(buffer);
+	private void answerWithEcho(ByteBuffer buffer, SocketChannel client, String user) throws IOException {
+       
 	        if (new String(buffer.array()).trim().equals("POISON_PILL")) {
 	            client.close();
 	            System.out.println("Not accepting client messages anymore");
 	        }
 	        else {
-	        	String data = new String(buffer.array(), buffer.position(), buffer.limit()).trim();
+	        	String data = new String("connection allowed" + Integer.toString(PageManager.connection)).trim();
 	        	buffer.clear();
-	        	System.out.println(data);
-	        	writeChunkData(buffer, client, data);
+	        	writeChunkData(buffer, client, data, user, "0000");
 	            buffer.clear();
 	        }
 	}
@@ -78,29 +64,43 @@ public class Server {
  
         SocketChannel client = serverSocket.accept();
         client.configureBlocking(false);
-        client.register(selector, SelectionKey.OP_READ);
+        int interestSet = SelectionKey.OP_READ | SelectionKey.OP_WRITE; 
+        client.register(selector, interestSet);
     }
+    
+    public String byteToString(byte[] bytes) {
+		StringBuilder sb = new StringBuilder();
+		for (byte b: bytes) {
+			sb.append(String.format("%02X", b&0xff));
+		}
+		return sb.toString();
+	}
 	
-	public void readChunkData(ByteBuffer buffer, SocketChannel client) throws IOException{
-		 //SocketChannel client = (SocketChannel) key.channel();
-		 //read head
-		 ByteBuffer hbuffer = ByteBuffer.allocate(8);
-		 int rlen = client.read(hbuffer);
+	public String readChunkData(ByteBuffer buffer, SocketChannel client) throws IOException{		
+		ByteBuffer hbuffer = ByteBuffer.allocate(8);
+		buffer.clear();
+		
+		int rlen = client.read(hbuffer);
 		 if (rlen == -1) {
 			 throw new IOException("Socket closed");
 		 }
+		 else if(rlen == 0) {
+			 return "";
+		 }
 		 else if (rlen != 8) {
 			 String response = new String(hbuffer.array()).trim();
-	         System.out.println("response=" + response + " hlen: " + rlen);
+	         System.out.println("Server::Packet Head Corruption response=" + response + " hlen: " + rlen);
 			 throw new IOException("Packet Head Corruption");
 		 }
+		 this.code = new String(hbuffer.array(), 0, 4);
 		 int len =  Integer.parseInt((new String(hbuffer.array())).substring(4));
-		 int size = 0;
 		 
-		 //read data length
-		 //buffer = ByteBuffer.allocate(2048);
+		 len -= 64;		 
+		 int size = 0;
+
+		 //read data length	 
 		 while(size < len) {
-			 if((rlen = client.read(buffer)) == -1) {
+ 			 if((rlen = client.read(buffer)) == -1) {
 				 throw new IOException("Socket closed");
 			 }
 			 size += rlen;
@@ -108,25 +108,189 @@ public class Server {
 		 
 		 //make read mode
 		 buffer.flip();
-		 
+		 int curr = buffer.position();
+		 this.curuser = new String(buffer.array(), curr, curr+64);
+		 String msg = new String(buffer.array(), curr+64, curr+len);
+		 return msg.trim();
 	}
 	
-	public void writeChunkData(ByteBuffer buffer, SocketChannel client, String data) throws IOException{
+	
+	
+	public void writeChunkData(ByteBuffer buffer, SocketChannel client, String data, String user, String code) throws IOException{
 		//SocketChannel client = (SocketChannel) key.channel();
-
+		System.out.println("System::sendmsg=" + data);
+		buffer.clear();
 		//make app data head
-		System.out.println(data);
-		String hlen = String.format("%04d", data.trim().length());
-		data = "0000" + hlen + data;
 		//System.out.println(data);
+		String hlen = String.format("%04d", data.trim().length() + 64);
+		String data_head = code + hlen;
+		byte pad = 0x00;
+		//System.out.println(data + " " + data_head);
 		
+		buffer.put(data_head.getBytes());
+		buffer.put(user.getBytes());
+		int rest = 64 - user.getBytes().length;
+		for(int i=0; i < rest; i++) buffer.put(pad);
 		buffer.put(data.getBytes());
 		buffer.flip();
-		
+		//System.out.println(buffer);
+		//System.out.println(byteToString(buffer.array()));
 		
 		while(buffer.hasRemaining()) {
 			client.write(buffer);
 		}
+	}
+	
+	public void updateGo(String msg) {
+		char[] state = msg.toCharArray();
+		System.out.println("System::received=" + msg);
+		this.turn = state[1] == '1' ? '2':'1';
+		for(int i=0; i < 361; i++) {
+			this.stateArr[i] = state[i+2];
+		}
+	}
+	
+	public void sendGomsg(SocketChannel client, String user) throws IOException{		
+		String smsg = String.valueOf(this.turn) + "0" + String.valueOf(stateArr);
+		writeChunkData(this.buffer, client, smsg, user, "2003");
+	}
+
+	
+	public void startS () throws Exception{
+		while (!PageManager.over) {
+            selector.select();
+            Set<SelectionKey> selectedKeys = selector.selectedKeys();
+            Iterator<SelectionKey> iter = selectedKeys.iterator();
+            SocketChannel client = null;
+            String user = new String("");
+            while (iter.hasNext()) {
+
+                SelectionKey key = iter.next();         
+                try {
+	                if (key.isAcceptable()) {
+	                    register(selector, serverSocket);             
+	                }
+                
+                
+                	if (key.isReadable()) {
+                    	client = (SocketChannel) key.channel();
+            	        
+            	        String msg = readChunkData(buffer, client);
+            	        user = this.curuser;
+            	        if (msg == "") continue; //non blocking so it just return 0 when there is no packets
+            	        //System.out.println(msg);
+                    	if(PageManager.gameN == 6) {
+                    		if(PageManager.connection < 2 && msg.equals("hello")) { //before game start
+                    			PageManager.connection += 1;
+                    			this.users.add(user);
+                    			
+                    			System.out.println("Server::connections : " +PageManager.connection);
+                    			buffer.clear();
+                        		answerWithEcho(buffer, client, user);
+                        		if(PageManager.connection == 2) {   
+                        			this.gamestate = 1;
+                        			Thread.sleep(100);
+                        			writeChunkData(buffer, client, "start", "Server", "2001");
+                        			this.done += 1;
+                        			int i = 0;
+                        			//Set<SelectionKey> selectedKeys2 = selector.selectedKeys();
+                                    //Iterator<SelectionKey> iter2 = selectedKeys2.iterator();
+                        			iter.remove();
+                        			while(iter.hasNext()) {
+                        				SelectionKey key2 = iter.next();
+                        				System.out.println(i);
+                        				if(key2.isWritable()) {
+                        					SocketChannel clientr = (SocketChannel) key2.channel();
+                            				writeChunkData(buffer, clientr, "start", "Server", "2001");                              				
+                        				}
+                        				iter.remove();
+                        				this.done += 1;
+                        				if (this.done == 2) this.gamestate = 3;
+                        			}
+                        			this.turn = '1';
+                        			System.out.println("Server::game2 start!!");
+                        			continue;
+                        		}
+                   
+                    		}
+                    		else if(user != "" && users.contains(user) && PageManager.connection == 2){ //running on the game
+                    			//implement game logic
+                    			System.out.println("System:: code " + this.code + " msg= " +msg);
+                    			if(this.code.equals("2003")) {
+                    				updateGo(msg);
+                    				//System.out.println("Server::recivedmsg="+msg);
+                        			sendGomsg(client, user);
+                    			}
+                    		}	
+                        	else { //out of connection
+                        		System.out.println(user + " " + users.contains(user));
+                        		Iterator<String> iterator = users.iterator();
+                                while (iterator.hasNext()) {
+                                    System.out.print(iterator.next() + "  ");
+                                }
+                                System.out.println();
+                        		writeChunkData(buffer, client, "connection is refused", "Server", "0000");
+                        		client.close();
+                        		continue;
+                        	}
+                      	
+                        }
+                        
+                        else { //if not page6
+                        	PageManager.page = 1;
+                        	return;
+                        }
+           	
+                    }
+                	else if (key.isWritable()) {
+                		client = (SocketChannel) key.channel();
+                		if(this.gamestate == 1) {
+                			writeChunkData(buffer, client, "start", "Server", "2001");
+                			this.done += 1;
+            				if (this.done == 2) this.gamestate = 3;
+                		} 
+                		else if(this.gamestate == 3) {
+                			sendGomsg(client, user);
+                		}
+	                }
+                	
+                }
+                catch(Exception e) {
+                	count += 1;
+                	if(client != null) {
+                		e.printStackTrace();
+                		
+                		PageManager.connection = 0;
+                		//PageManager.page = 1;
+                		PageManager.over = true;
+
+                		client.close();
+                		iter.remove();
+            			while(iter.hasNext()) {
+            				//System.out.println(i);
+            				SelectionKey keyr = iter.next();
+            				if(key.isWritable()) {
+            					SocketChannel clientr = (SocketChannel) keyr.channel();
+                				writeChunkData(buffer, clientr, "end", "Server", "2002");
+                				clientr.close();
+            				}
+            				iter.remove();
+            			}
+            			
+            			continue;
+                	}
+                	else e.printStackTrace();
+                }
+
+                
+                iter.remove();
+            }
+            
+            if(count > 10) return;
+            Thread.sleep(500);
+		}
+		
+		PageManager.page = 1;
 	}
 	
 	
