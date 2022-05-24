4f0ae0035a6cdf551f750db3a890299335f6f382
swjjnew <103331777+swjjnew@users.noreply.github.com>
2022-05-25 00:56:07 +0900

    Update Main.java

--- src/MiniGame/src/minigame/Main.java
+++ src/MiniGame/src/minigame/Main.java
@@ -1,54 +1,112 @@
 package minigame;
 
 @SuppressWarnings("serial")
-public class Main extends PageManager{
+public class Main extends PageManager implements Runnable{
+	Server server = new Server();
+	Client client = new Client();
+	StartPage page1 = new StartPage();
+	MakePartyPage page2 = new MakePartyPage();
+	Wait page3 = new Wait();
+	G2go page6 = new G2go(client);	
+	static Game1 page5;
+	
+	public Main() {
+		
+	}
+	
+	@Override
+    public void run() {
+		/* 스레드 실행코드 */
+		try {
+			if(PageManager.page == 4) {
+				System.out.println("new thread for server");
+				this.server.startS();
+				page3.setvisibility(false);
+				System.out.println("close the server");
+			}
+			else if(PageManager.page == 44) {
+				System.out.println("new thread for game2");
+				this.page6.G2Run();
+				this.page6.setvisibility(false);	
+			}
+			
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			PageManager.page = 2;	
+		}
+		
+    }
+	
 	public static void main(String args[]) {
-		Server server = new Server();
-		Client client = new Client();
-		StartPage page1 = new StartPage();
-		MakePartyPage page2 = new MakePartyPage();
-		Wait page3 = new Wait();
-		Game1 page5;
+		Main main = new Main();
 		
 		while(!finish) {
 			int pageN = PageManager.page;
 			
-			if(PageManager.page == 1) page1.setvisibility(true);
-			if(PageManager.page == 2) page2.setvisibility(true);
+			if(PageManager.page == 1) main.page1.setvisibility(true);
+			if(PageManager.page == 2) main.page2.setvisibility(true);
 			if(PageManager.page == 5) {
 				page5 = new Game1();
 				page5.setvisibility(true);
 				page5.Start();
+				page5 = null;
 			}
 			if(PageManager.page == 3) {
-				page3.setvisibility(true);
+				main.page3.setvisibility(true);
 				
 				try {
+					PageManager.page = 4;
 					if(PageManager.ip == "") { //server
+						main.server.init(PageManager.portS);
+						Thread subTread1 = new Thread(main);
+						subTread1.start();
+						//participate as client too. do not make global memory
+						//implement this
 						
-						server.init(PageManager.portS);
-						//implement check the success of connection
+						main.client.init("127.0.0.1", PageManager.portS);
+						//check connection number
+						//synchronize data along clients
+						//implement game algorithm.
+						//after game over terminate above thread.
+						main.client.sendMessage("hello", PageManager.id, "0000");	
+						String msg = main.client.receiveMessage().trim();
+						if (msg.contains("connection allowed")) {
+							PageManager.page = 44;
+							Thread subTread2 = new Thread(main);
+							PageManager.state = msg.charAt(msg.length() - 1);
+							subTread2.start();
+						}
+						else PageManager.page = 2;
 					}
 					else { //client
-						client.init(PageManager.ip, PageManager.portC);
+						
+						main.client.init(PageManager.ip, PageManager.portC);
 						//have to implement actions
-						client.sendMessage("hello");
-						client.sendMessage("This is the test");
-						client.sendMessage("by~");
+						main.client.sendMessage("hello", PageManager.id, "0000");						
+						String msg = main.client.receiveMessage().trim();
+						if (msg.contains("connection allowed")) {
+							PageManager.page = 44;
+							Thread subTread2 = new Thread(main);
+							PageManager.state = msg.charAt(msg.length() - 1);
+							subTread2.start();
+						}
+						else PageManager.page = 2;
 					}
-					PageManager.page = 4;
 				}
 				catch (Exception e) {
 					e.printStackTrace();
-					PageManager.page = 2;
-					
+					PageManager.page = 2;		
 				}
-				page3.setvisibility(false);
-				// have to implement game2 and game 3
 				
 			}
+			
 			//have to implement game2 game3 page
-			if(PageManager.page == 4) page1.setvisibility(true);
+			if(PageManager.page == 6) {
+				System.out.println("game2");
+				main.page6.setvisibility(true);	
+				main.page3.setvisibility(false);
+			}
 			
 			while(pageN == PageManager.page) {
 				try {
@@ -59,6 +117,5 @@ public class Main extends PageManager{
 				}
 			}
 		}
-		
 	}
 }
